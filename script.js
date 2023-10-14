7fa375948bbc0703c50abb03242c5c222d7bcf33
Kushank Rajput <143720436+KushankRajput@users.noreply.github.com>
2023-10-14 13:03:14 +0530

    upload some files
    upload some files

---
+++ script.js
@@ -0,0 +1,35 @@
+let typeData = new Typed(".role", {
+    strings: [
+      "Full Stack Developer",
+      "Web Developer",
+      "Software Engineer",
+      "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
+
+  let insta;
+    document.querySelector(".insta").addEventListener("click",function(){
+      insta= window.open('https://www.instagram.com/kush111603/');
+  });
+
+ let call= document.querySelector(".call").addEventListener("click",function(){
+  document.write("Call me =>+91 93680 12393")});
+
+    let git;
+    document.querySelector(".git").addEventListener("click",function(){
+      git= window.open('https://github.com/KushankRajput');
+    });
+
+    let twit;
+    document.querySelector(".twit").addEventListener("click",function(){
+        twit= window.open("https://twitter.com/KushankRajput16?t=YlEvEHhI2wyNgDVcomOhxw&s=09");
+    });
+
+    let resp= document.querySelector(".kush").addEventListener("click",function(){
+      location.href="C:\Users\kusha\OneDrive\Desktop\CSS\portfolio web\my web\web.html"
+    });
+
